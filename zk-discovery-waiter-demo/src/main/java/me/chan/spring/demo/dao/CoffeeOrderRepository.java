3a69ecc69833c167dd8b2feba363fb266c175dab
jangin <progchen295@gmail.com>
2021-03-01 17:35:38 +0800

    add zookeeper discovery waiter demo

---
+++ zk-discovery-waiter-demo/src/main/java/me/chan/spring/demo/dao/CoffeeOrderRepository.java
@@ -0,0 +1,7 @@
+package me.chan.spring.demo.dao;
+
+import me.chan.spring.demo.model.CoffeeOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoffeeOrderRepository extends JpaRepository<CoffeeOrder, Long> {
+}
